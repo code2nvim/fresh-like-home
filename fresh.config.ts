247816fbc53254fe56f43a29ba7344f4e5659937
code2nvim <code2nvim@gmail.com>
2025-05-21 23:03:28 +0800

    add HTTPS support

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import { readTextFile } from "jsr:@std/fs/unstable-read-text-file";
 
 export default defineConfig({
   plugins: [tailwind()],
+  server: {
+    cert: await readTextFile(".local/public_certificate.pem"),
+    key: await readTextFile(".local/private_key.pem"),
+  },
 });
