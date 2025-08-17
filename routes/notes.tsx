b574709fafac771001aa27920b77ea56bf34b761
code2nvim <code2nvim@gmail.com>
2025-08-18 00:48:15 +0800

    create basic layout for notes

--- routes/notes.tsx
+++ routes/notes.tsx
@@ -1,5 +1,10 @@
-import { PageProps } from "$fresh/server.ts";
-
-export default function Notes(props: PageProps) {
-  return <main class="mx-auto grow">Page: {props.url.href}</main>;
+export default function Notes() {
+  return (
+    <main class="flex w-full grow justify-around">
+      <nav class="m-1 w-1/5 rounded-md border">nav</nav>
+      <section class="itemrs-center m-1 flex grow rounded-md border">
+        notes
+      </section>
+    </main>
+  );
 }
